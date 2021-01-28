aa7503e18b75dfe8e02500a330ea34e0306131a7
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-01-28 20:38:51 +0545

    added a file around views and its theories

--- Scripts/advanced-sql/Views.sql
+++ Scripts/advanced-sql/Views.sql
@@ -16,3 +16,32 @@
 
 -- deleting a view
 -- drop view [if exists] <view_name>
+
+-- find the last salary change of each employee into a view
+create or replace view last_sal_change as
+select
+	e.emp_no,
+	max(s.from_date) --as "last salary change date"
+from salaries s
+join employees e using(emp_no)
+join dept_emp de using(emp_no)
+join departments d using(dept_no)
+group by e.emp_no
+order by e.emp_no;
+
+-- change the name of max salary change column
+alter view last_sal_change rename column max to "last salary change date";
+
+-- now query this view: get same info back
+select * from last_sal_change; 
+
+-- join with salaries table to find latest/current salary
+select 
+	s.emp_no,
+	s.salary,
+	s.from_date,
+	s.to_date 
+from salaries s 
+join last_sal_change as lsc using (emp_no)
+where s.from_date = lsc."last salary change date"
+order by s.emp_no 
